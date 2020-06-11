bd9cda5c2bb8fbc954b2aa58be6ccc5489378cb3
Kartik Pandey <kartik.pandey@nagarro.com>
2020-06-11 22:03:28 +0530

    section 2- lecture 10. Setted up basic API project with entity class

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController: ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts(){
+            return "list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id){
+            return "this is the product";
+        }
+    }
+}
