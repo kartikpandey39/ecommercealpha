ad95969091b70ca34fb32c026a8fa85a2b6f5291
Kartik Pandey <kartik.pandey@nagarro.com>
2020-06-14 01:04:38 +0530

    end of section 2 - with basic project infrastructure

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,19 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController: ControllerBase
+    public class ProductsController : ControllerBase
     {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
-        public string GetProducts(){
-            return "list of products";
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            // sync requests
+            //var products = _context.Products.ToList();
+
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public string GetProduct(int id){
-            return "this is the product";
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
         }
     }
 }
